03e950285c673ab198b45dfba20f5050b062af68
Leon <liang.li@charleskeith.com>
2019-06-28 13:44:54 +0800

    add favorite in detail

--- Landmarks/LandmarkDetail.swift
+++ Landmarks/LandmarkDetail.swift
@@ -24,8 +24,23 @@ struct LandmarkDetail : View {
                 .offset(y: -130)
                 .padding(.bottom, -130)
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    Button(action: {
+                        self.userData.landmarks[self.landmarkIndex].isFavorite.toggle()
+                    }) {
+                        if self.userData.landmarks[self.landmarkIndex].isFavorite {
+                            Image(systemName: "star.fill")
+                                .imageScale(.large)
+                                .foregroundColor(Color.yellow)
+                        } else {
+                            Image(systemName: "star")
+                                .imageScale(.large)
+                                .foregroundColor(Color.gray)
+                        }
+                    }
+                }
                 HStack {
                     Text(landmark.park)
                         .font(.subheadline)
