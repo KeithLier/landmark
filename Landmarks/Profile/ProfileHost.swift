bb56f941a50c7619b8a6d87c950ae54d3c005e70
Leon <liang.li@charleskeith.com>
2019-07-09 08:06:27 +0800

    finish tutorial must review

--- Landmarks/Profile/ProfileHost.swift
+++ Landmarks/Profile/ProfileHost.swift
@@ -9,11 +9,34 @@
 import SwiftUI
 
 struct ProfileHost : View {
+    @Environment(\.editMode) var mode
     @State var profile = Profile.default
+    @State var draftProfile = Profile.default
     
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
-            ProfileSummary(profile: self.profile)
+            HStack {
+                if self.mode?.value == .active {
+                    Button(action: {
+                        self.profile = self.draftProfile
+                        self.mode?.animation().value = .inactive
+                    }) {
+                        Text("Done")
+                    }
+                }
+                
+                Spacer()
+                
+                EditButton()
+            }
+            if self.mode?.value == .inactive {
+                ProfileSummary(profile: self.profile)
+            } else {
+                ProfileEditor(profile: $draftProfile)
+                    .onDisappear {
+                        self.draftProfile = self.profile
+                    }
+            }
         }
         .padding()
     }
